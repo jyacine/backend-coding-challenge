16674ba4258cec626950eebf8a09db37f81fce45
xdgt0500 <yacine.jaber@orange.com>
2021-05-02 18:11:33 +0000

    update number of items returned

--- src/main/java/com/gemography/challenge/svc/GithubTrendingService.java
+++ src/main/java/com/gemography/challenge/svc/GithubTrendingService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,9 @@ public class GithubTrendingService implements ITrendingService {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Value("${coding.challenge.github.nb_items}")
+	private int nb_items; 
+	
 	private static final String githubUrl = "https://api.github.com/search/repositories"
 			+ " ?q=created:>{date}&sort=stars&order=desc";
 	
@@ -27,7 +31,7 @@ public class GithubTrendingService implements ITrendingService {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");		
 		Map<String, String> uriParams = new HashMap<String, String>();
 		uriParams.put("date", sdf.format(date));
-		GithubReposDTO response = restTemplate.getForObject(githubUrl, GithubReposDTO.class,uriParams); 
+		GithubReposDTO response = restTemplate.getForObject(githubUrl+"&per_page="+String.valueOf(nb_items), GithubReposDTO.class,uriParams); 
 		return response;
 	}
 
